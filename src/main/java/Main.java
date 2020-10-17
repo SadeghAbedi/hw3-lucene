f3b46b0503e5b3dff0aa9932823b673af4730c06
Parsa <salehipour.edu@gmail.com>
2020-10-17 13:03:59 +0330

    trying to create index

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,31 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        DocumentIndexer indexer = null;
+        System.out.println("salam" );
         try {
-            indexer = new DocumentIndexer();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            String indexPath = System.getProperty("user.dir").concat("/indexes");
+            System.out.println(indexPath);
+            Directory dir = FSDirectory.open(Paths.get(indexPath));
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig indexWriterConfig  = new IndexWriterConfig( analyzer);
+            IndexWriter writer = new IndexWriter(dir , indexWriterConfig);
 
-        Arrays.stream(args).filter(s -> s.matches("\\d+-\\d+"))
-                .map(s -> {
-            var range = s.split("-");
-            return IntStream.range(Integer.parseInt(range[0]),Integer.parseInt(range[1]));
-        }).flatMap(IntStream::boxed).forEach(indexer::addDocumentNumber);
-        indexer.perform();
+            writer.close();
+        } catch (IOException ex){
+            System.out.println(ex.getStackTrace());
+
+        }
     }
 }
