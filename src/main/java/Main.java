4cbfa329e43381a15ddbefcd1514e3a157af8ddc
Parsa <salehipour.edu@gmail.com>
2020-10-17 16:35:37 +0330

    previous commit conflicts resolved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,35 +11,27 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("salam" );
+        DocumentIndexer indexer = null;
         try {
-            String indexPath = System.getProperty("user.dir").concat("/indexes");
-            System.out.println(indexPath);
-            Directory dir = FSDirectory.open(Paths.get(indexPath));
-            Analyzer analyzer = new StandardAnalyzer();
-            IndexWriterConfig indexWriterConfig  = new IndexWriterConfig( analyzer);
-            IndexWriter writer = new IndexWriter(dir , indexWriterConfig);
+            indexer = new DocumentIndexer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-<<<<<<< Updated upstream
-            writer.close();
-        } catch (IOException ex){
-            System.out.println(ex.getStackTrace());
-
-=======
         Arrays.stream(args).filter(s -> s.matches("\\d+-\\d+"))
                 .map(s -> {
-            var range = s.split("-");
-            return IntStream.range(Integer.parseInt(range[0]),Integer.parseInt(range[1]));
-        }).flatMap(IntStream::boxed).forEach(indexer::addDocumentNumber);
+                    var range = s.split("-");
+                    return IntStream.range(Integer.parseInt(range[0]),Integer.parseInt(range[1]));
+                }).flatMap(IntStream::boxed).forEach(indexer::addDocumentNumber);
         indexer.perform();
+
 
         try {
             DocumentIndexer.readIndex();
         } catch (IOException e) {
             e.printStackTrace();
->>>>>>> Stashed changes
         }
     }
-}
